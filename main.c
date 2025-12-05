5b3e1cd82ec7ec8cd44521c7cdd0e534dd5162b2
shakekan24 <omura12248902@gmail.com>
2025-12-05 10:27:40 +0800

    feat: add addition operation

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 # include <stdio.h>
 int main() {
-printf("=== C Language Calculator ===\ n");
-/ / Operations placeholders
-printf("System Check OK.\ n");
+printf("=== C Language Calculator === n");
+
+int a = 10, b = 20;
+    printf("Add Result: %d\n", a + b);
+
+printf("System Check OK. n");
 return 0;
 }
