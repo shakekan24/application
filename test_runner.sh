de381ead800c969ab86a6fca412fb1365af122f0
shakekan24 <111269868+shakekan24@users.noreply.github.com>
2025-12-05 10:47:20 +0800

    Update test_runner.sh

--- test_runner.sh
+++ test_runner.sh
@@ -1,1 +1,12 @@
-
+# !/ bin/ bash
+echo " Running Build Test..."
+gcc main.c -o app
+if [ $? -eq 0 ]; then
+echo " Compilation Successful!"
+./ app
+echo " Execution Test Passed!"
+exit 0
+else
+echo " Compilation Failed!"
+exit 1
+f
